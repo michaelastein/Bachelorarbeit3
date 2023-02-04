444631f8ed52685ce634a3ca45f11cf3db1c9fba
Micha <Micha@LAPTOP-B7HBNHOG>
2023-02-04 14:27:41 +0100

    Projektdateien hinzufügen.

---
+++ materialfinder3/materialapp/apps.py
@@ -0,0 +1,11 @@
+from django.apps import AppConfig
+
+
+class MaterialappConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "materialapp"
+
+
+class MaterialappConfigdjango_api_view(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "materialapp"
---
+++ materialfinder3/materialapp/urls.py
@@ -0,0 +1,10 @@
+
+from django.urls import re_path
+
+from . import views 
+ 
+urlpatterns = [ 
+    re_path(r'^api/tutorials$', views.tutorial_list),
+    re_path(r'^api/tutorials/(?P<pk>[0-9]+)$', views.tutorial_detail),
+    re_path(r'^api/tutorials/published$', views.tutorial_list_published)
+]
