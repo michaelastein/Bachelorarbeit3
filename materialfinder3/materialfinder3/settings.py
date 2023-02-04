031bb441e56c35d0559731348fac35fb9e10f88b
Micha <Micha@LAPTOP-B7HBNHOG>
2023-02-04 16:00:31 +0100

    wie vorher, cors = True

--- materialfinder3/materialfinder3/settings.py
+++ materialfinder3/materialfinder3/settings.py
@@ -127,7 +127,7 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
-CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_ALLOW_ALL = True
 #false ist besser
 
 CORS_ORIGIN_WHITELIST = (
